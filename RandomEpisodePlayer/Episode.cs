f698c3a6586eb3266fde234da8456670519f439d
Jonathan Webb <jwebbed@bell.net>
2013-12-05 14:58:05 -0800

    Initial Commit

---
+++ RandomEpisodePlayer/Episode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RandomEpisodePlayer
+{
+    class Episode
+    {
+        private string name;
+        private string path;        
+
+        public Episode(string path)
+        {
+            this.path = path;
+        }
+
+        public Episode(string path, string name)
+        {
+            this.path = path;
+            this.name = name;
+        }
+
+        public void play()
+        {
+            System.Diagnostics.Process.Start(this.path);
+        }
+    }
+}
