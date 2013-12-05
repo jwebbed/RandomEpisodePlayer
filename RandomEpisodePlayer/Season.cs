d597c9456320d67e1ebff058e1174facb9aef14d
Jonathan Webb <jwebbed@bell.net>
2013-12-05 15:18:08 -0800

    Added season class

---
+++ RandomEpisodePlayer/Season.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomEpisodePlayer
+{
+    class Season : IEnumerable<Episode>
+    {
+        private int number;
+        private List<Episode> episodes;
+
+        List<Episode> Episodes{ get { return this.episodes; } }
+
+        public Season(int number){
+            this.number = number;
+            this.episodes = new List<Episode>();
+        }
+
+        public Season(int number, List<Episode> list)
+        {
+            this.number = number;
+            this.episodes = list;
+        }
+
+        public void addEpisode(Episode e)
+        {
+            this.episodes.Add(e);
+        }
+
+        public IEnumerator<Episode> GetEnumerator()
+        {
+            return this.episodes.GetEnumerator();
+        }
+        
+    }
+}
